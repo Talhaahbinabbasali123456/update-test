c4e56b78763113f9093d7f831001cd09b570318d
Talhaah Abbas <38943204+Talhaahbinabbasali123456@users.noreply.github.com>
2018-10-06 15:14:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// Write all your Javascript here!
+let month = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+function onloadFunc() {
+    $.ajax({
+        url: "https://techkaro-test.herokuapp.com/api/v1/getdata?lang=en",
+        success: function(data) {
+            console.log(data);
+            document.querySelector(`#img`).src = data.imageURL;
+            document.querySelector(`#heading`).innerHTML = data.info.heading;
+            document.querySelector(`#description`).innerHTML = data.info.description;
+            document.querySelector(`#heading2`).innerHTML = data.sale.heading;
+            document.querySelector(`#description2`).innerHTML = data.sale.description;
+            document.querySelector(`#i1`).innerHTML = data.menuOptions[0];
+            document.querySelector(`#i2`).innerHTML = data.menuOptions[1];
+            document.querySelector(`#i3`).innerHTML = data.menuOptions[2];
+            document.querySelector(`#i4`).innerHTML = data.menuOptions[3];
+            var Ndate = new Date(data.date);
+            var d = Ndate.getDay(Ndate * 1000);
+            var dat = Ndate.getMonth(Ndate * 1000);
+            var year = Ndate.getFullYear(Ndate * 1000);
+            var h = Ndate.getHours();
+            var m = Ndate.getMinutes();
+            document.querySelector(`#date`).innerHTML = `${month[dat]} ${d}, ${year} - ${h}H ${m}M`;
+        }
+    });
+}
+
+function callback(index) {
+    $.ajax({
+        url: `https://techkaro-test.herokuapp.com/api/v1/getdata?lang=${index}`,
+        success: function(data) {
+            console.log(data);
+            document.querySelector(`#img`).src = data.imageURL;
+            document.querySelector(`#heading`).innerHTML = data.info.heading;
+            document.querySelector(`#description`).innerHTML = data.info.description;
+            document.querySelector(`#heading2`).innerHTML = data.sale.heading;
+            document.querySelector(`#description2`).innerHTML = data.sale.description;
+            document.querySelector(`#i1`).innerHTML = data.menuOptions[0];
+            document.querySelector(`#i2`).innerHTML = data.menuOptions[1];
+            document.querySelector(`#i3`).innerHTML = data.menuOptions[2];
+            document.querySelector(`#i4`).innerHTML = data.menuOptions[3];
+            var Ndate = new Date(data.date);
+            var d = Ndate.getDay(Ndate * 1000);
+            var dat = Ndate.getMonth(Ndate * 1000);
+            var year = Ndate.getFullYear(Ndate * 1000);
+            var h = Ndate.getHours();
+            var m = Ndate.getMinutes();
+            document.querySelector(`#date`).innerHTML = `${month[dat]}-${d}-${year}  ${h}H : ${m}M`;
+        }
+    });
+}
